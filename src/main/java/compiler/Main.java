64b59fdec8385165707831ca601caf76b96cbb44
soltani <m.soltani.252@gmail.com>
2022-05-03 19:12:28 +0430

    remove redunant code in Main

--- src/main/java/compiler/Main.java
+++ src/main/java/compiler/Main.java
@@ -8,30 +8,23 @@ import compiler.scanner.Preprocessor;
 
 public class Main {
 
-    public static String run(java.io.File inputFile) throws Exception {
-        StringBuilder str = new StringBuilder();
-
+    public static boolean run(java.io.File inputFile) {
         Preprocessor preprocessor = new Preprocessor(inputFile, ".pp_tmp");
         preprocessor.preprocess();
-
         inputFile = new File(".pp_tmp");
-        Lexer scanner = new Lexer(new FileReader(inputFile));
-        while (scanner.hasToken()) {
-            Symbol token = scanner.next_token();
-            if (token.sym == 0)
-                break;
-            str.append(scanner.getTokenString(token) + "\n");
+
+        Lexer scanner;
+        try {
+            scanner = new Lexer(new FileReader(inputFile));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
         }
-        return str.toString();
+        // TODO: add parser
+        return true;
     }
 
     public static void main(String[] args) {
-        String output = "";
-        try {
-            output = run(new File("input.txt"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        System.out.print(output);
+        System.out.println(run(new File("input.txt")));
     }
 }
